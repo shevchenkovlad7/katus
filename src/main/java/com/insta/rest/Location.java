c37ebd458ac42f44cd1383dbc6f0c81087a83a0b
Vlad <shevchenkovlad777@gmail.com>
2016-05-14 20:07:51 +0300

    rest Location.java

--- src/main/java/com/insta/rest/Location.java
+++ src/main/java/com/insta/rest/Location.java
@@ -1,10 +1,25 @@
 package com.insta.rest;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by gleb on 14.05.16.
  */
-@Path("/")
+@Path("/locations")
 public class Location {
+
+    @Path("/{paramX}/{paramY}")
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public Response getLocation(@PathParam("paramX") String paramX, @PathParam("paramY") String paramY) {
+        if (paramX == null || paramY == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.ok().entity("Hello word!").build();
+    }
 }
