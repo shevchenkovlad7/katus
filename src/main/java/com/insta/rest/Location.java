2fb8214b766447e3e91a635dc4a9d83c4150683b
Vlad <shevchenkovlad777@gmail.com>
2016-05-15 05:04:24 +0300

    Log4j

--- src/main/java/com/insta/rest/Location.java
+++ src/main/java/com/insta/rest/Location.java
@@ -15,6 +15,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.apache.log4j.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -28,12 +29,13 @@ import java.util.List;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+
 /**
  * Created by gleb on 14.05.16.
  */
 @Path("/locations")
 public class Location {
-
+    private static final Logger log = Logger.getLogger(Location.class);
     private ObjectMapper objectMapper = new ObjectMapper();
     private String url;
     private String status;
@@ -41,16 +43,18 @@ public class Location {
     private BufferedReader bufferedReader;
     private List<String> list;
 
-
     @Path("/{paramX}/{paramY}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getLocation(@PathParam("paramX") String paramX, @PathParam("paramY") String paramY) throws IOException {
+        log.debug("Start getLocation for Pram X and Y :" + paramX +", " + paramY);
 
         url = "https://api.instagram.com/v1/locations/search?lat=" + paramX + "&lng=" + paramY + "&access_token=" + token;
+        log.debug("Token : "+token);
         list = new ArrayList<>();
 
         if (paramX == null || paramY == null) {
+            log.warn("paramX or paramY null :" +paramX + ", "+ paramY);
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
 
@@ -82,18 +86,21 @@ public class Location {
         List<ResponseObject> responseObjects = new ArrayList<>();
 
         JSONObject jsonObject = new JSONObject(jsonLocations);
+        log.debug("Create jsonLocation :" + jsonLocations);
         JSONArray locations = jsonObject.getJSONArray("data");
         for (Object o : locations) {
             try {
                 ResponseObject responseObject = new ResponseObject();
                 LocationObject locationObject = objectMapper.readValue(o.toString(), LocationObject.class);
+                log.debug("SetLocation :"+locationObject);
                 responseObject.setLocationObject(locationObject);
                 responseObjects.add(responseObject);
             } catch (IOException e) {
                 e.printStackTrace();
+                log.warn("Exeption");
             }
         }
-
+        log.debug("Send in urlPhoto :"+ responseObjects);
         urlPhotos(responseObjects);
 
         return Response.ok().entity(responseObjects).build();
@@ -105,15 +112,20 @@ public class Location {
 
             DefaultHttpClient hc = new DefaultHttpClient();
             ResponseHandler response = new BasicResponseHandler();
+
             HttpGet http = new HttpGet("https://www.instagram.com/explore/locations/" + responseObject.getLocationObject().getId());
+            log.debug("Get location by id"+responseObject.getLocationObject().getId());
             String html = "";
             try {
+                log.debug("string html");
                 html = (String) hc.execute(http, response);
             } catch (IOException e) {
                 e.printStackTrace();
+                log.warn("Exeption");
             }
-
+            log.debug("parse html");
             Document document = Jsoup.parse(html);
+            log.debug("Script element change");
             Elements elements = document.getElementsByTag("script");
 
             JSONObject jsonObject = null;
@@ -123,6 +135,7 @@ public class Location {
                 if (json.startsWith("window._sharedData")) {
                     String s = json.substring(json.indexOf("{"), json.lastIndexOf("}") + 1);
                     jsonObject = new JSONObject(s);
+                    log.debug("Created JSON");
                 }
             }
 
@@ -140,6 +153,7 @@ public class Location {
                 System.out.println(link);
                 imeges.add(link);
             }
+            log.debug("Add images");
             responseObject.setImegUrls(imeges);
         }
 
