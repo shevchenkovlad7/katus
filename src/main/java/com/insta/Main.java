44f9c4658a9860e046b019e0d076f1b7f2b58f69
Vlad <shevchenkovlad777@gmail.com>
2016-05-15 03:42:35 +0300

    m

--- src/main/java/com/insta/Main.java
+++ src/main/java/com/insta/Main.java
@@ -1,7 +1,20 @@
 package com.insta;
 
+import com.insta.rest.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gleb on 14.05.16.
  */
 public class Main {
+    public static void main(String[] args) {
+        Location location = new Location();
+        List<String> list = new ArrayList<>();
+
+        location.urlPhotos(list);
+
+
+    }
 }
--- src/main/java/com/insta/rest/Location.java
+++ src/main/java/com/insta/rest/Location.java
@@ -1,11 +1,28 @@
 package com.insta.rest;
 
+
+import jdk.nashorn.internal.parser.JSONParser;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.codehaus.jettison.json.JSONObject;
+import org.w3c.dom.Document;
+
+import javax.validation.constraints.AssertFalse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Created by gleb on 14.05.16.
@@ -21,5 +38,23 @@ public class Location {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
         return Response.ok().entity("Hello word!").build();
+    }
+    public List<String> urlPhotos(List<String> list){
+        DefaultHttpClient hc = new DefaultHttpClient();
+        ResponseHandler response = new BasicResponseHandler();
+        HttpGet http = new HttpGet("https://www.instagram.com/explore/locations/217054655/");
+        String html = "";
+        try {
+            html =(String) hc.execute(http, response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.print(html);
+
+        Document doc = Jsoup.parse(html);
+
+
+
+        return list;
     }
 }
